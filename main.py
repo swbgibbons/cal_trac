dd182694940bb93fc4188934ea678e9499874456
swbgibbons <84992776+swbgibbons@users.noreply.github.com>
2022-09-16 12:58:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from Food_Bank import *
 
 if __name__ == '__main__':
 
-    bkfast = sum([coffee,ldu_marco_pollo,
-                  reeses_egg*2])
+    bkfast = sum([coffee,bkfast_burrito,protein_shake,
+                  banana,fairlife_choc_milk])
     
     bkfast.name = 'Breakfast'
     
-    lunch = sum([red_robins_scorpion_burger,red_robins_yam_fries,
-                 lakewood_lager_12oz*2])
+    lunch = sum([chicken_breast_6oz,butter,yams_100g*2,brussels_100g])
     lunch.name = 'Lunch'
 
-    dinner = sum([modelo_24oz*2,buttermilk_sky_pecan_pie])
+    dinner = sum([salmon_4oz,butter/2,white_rice_6oz*2,broccoli_100g,
+                  modelo_24oz])
     dinner.name = 'Dinner'
 
     net_total = sum([bkfast,lunch,dinner])
